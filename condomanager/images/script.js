20548dc1900a8016e90759f828fb2bc118948438
Fabio <159775445+fdelse@users.noreply.github.com>
2026-01-12 12:41:36 +0100

    upload

---
+++ condomanager/images/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Sito Estia inizializzato con successo.");
+    
+    const header = document.querySelector('.ct-header');
+    if(header) {
+        console.log("Layout pronto e sito completo caricato.");
+    }
+});
