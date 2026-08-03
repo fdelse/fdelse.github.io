402be6f5af043e83c1840e7b3d8e5446d3e6f051
Raquel-wq <cliente.raquel.cabello@gmail.com>
2026-08-03 17:26:05 +0200

    up

--- testita/test-data.js
+++ testita/test-data.js
@@ -8208,8 +8208,8 @@ const TEST_STRUCTURES = [
             "fix": "quindi"
           },
           {
-            "q": "Nevica e vado in bici lo stesso.",
-            "wrong": "e",
+            "q": "Nevica o vado in bici lo stesso.",
+            "wrong": "o",
             "fix": "ma"
           },
           {
